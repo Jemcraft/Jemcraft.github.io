69cf1d3b686d1779d3b5ed49e91c3ca786d449a7
mobeets <mobeets@gmail.com>
2016-06-25 17:25:22 -0400

    demo

--- js/table_ex.js
+++ js/table_ex.js
@@ -1,5 +1,21 @@
-window.onload = function() {
-    editableGrid = new EditableGrid("Assignments"); 
-    editableGrid.tableLoaded = function() { this.renderGrid("tablecontent", "testgrid table table-hover table-bordered"); };
-    editableGrid.loadJSON("data/example.json");
+function load_tables() {
+
+    editableGrid1 = new EditableGrid("Assignments"); 
+    editableGrid1.tableLoaded = function() { this.renderGrid("assigns", "testgrid grid-assigns table table-bordered"); };
+    editableGrid1.loadJSON("data/example_assign.json");
+
+    editableGrid2 = new EditableGrid("Jobs"); 
+    editableGrid2.tableLoaded = function() { this.renderGrid("jobs", "testgrid grid-jobs table table-bordered"); };
+    editableGrid2.loadJSON("data/example_jobs.json");
+
+    editableGrid3 = new EditableGrid("Users"); 
+    editableGrid3.tableLoaded = function() { this.renderGrid("users", "testgrid grid-users table table-bordered"); };
+    editableGrid3.loadJSON("data/example_users.json");
+
 }
+
+$( document ).ready(function() {
+
+    load_tables();
+
+});
